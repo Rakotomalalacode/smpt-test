579866daab86b4bff2067188b84e33588535b463
Rakotomalalacode <rakotomalalacode@gmail.com>
2025-09-22 13:07:05 +0300

    smtp

--- app/comfirm.tsx
+++ app/comfirm.tsx
@@ -10,14 +10,13 @@ export default function ConfirmSubscription() {
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold text-gray-800 mb-4">Abonnement confirmé !</h1>
         <p className="text-gray-700 mb-6">
-          Merci d'avoir confirmé votre abonnement à notre newsletter, <span className="font-semibold">{email}</span> !
+          Merci d&lsquo;avoir confirmé votre abonnement à notre newsletter, <span className="font-semibold">{email}</span> !
         </p>
-        <a
-          href="/"
+        <p
           className="bg-blue-600 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 transition-colors duration-300"
         >
-          Retour à l'accueil
-        </a>
+          Retour à l&#39;accueil
+        </p>
       </div>
     </div>
   );
